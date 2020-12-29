0988c191f9acd070637eb9476843a98d43b24223
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2020-12-29 11:42:18 +0330

    new nova changes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 $router->group(['prefix' => 'storage'], function ($router) {
 
